badd4ac05047db6b57dd720f199740febbd58ea7
anas <mranassallam@gmail.com>
2024-04-21 22:04:07 -0400

    Added new files

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    public Vector3 position;
+
+    public PlayerData(Player player)
+    {
+        //position = Vector3;
+
+    }
+}
