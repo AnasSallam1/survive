c3b813e9be402580bac4b6b3756d3fd4d6593185
anas <mranassallam@gmail.com>
2024-04-22 20:54:26 -0400

    Added new scenes and scripts.

---
+++ Assets/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start playing the game.
+    public void Play()
+    {
+        
+    }
+
+    // Start playing the game.
+    public void Settings()
+    {
+
+    }
+
+    // Start playing the game.
+    public void About()
+    {
+        
+    }
+
+    // Exit the game when the button Exit is pressed.
+    public void Exit()
+    {
+        Debug.Log("Exit the game.");
+        Application.Quit();
+    }
+}
