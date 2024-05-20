88d33bb03e6824b2df8e6205a6cb7b5f12bb9c90
Anas Sallam <Anas.Sallam001@umb.edu>
2024-05-20 02:39:06 +0000

    Added a GameManager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // A static private variable to hold the reference to this Game Manager instance.
+    private static GameManager _instance;
+    // A public static Property to allow other classes to get the refernece.
+    public GameManager static GameManager Instance {
+        get { 
+            if (_instance == null)
+                Debug.LogError("ERROR: No GameManager exists in scene.");
+            return _instance; 
+        }
+
+    }
+
+    // Set the _instance reference at the soonest opportunity when the game starts.
+    private void Awake() => _instance = this; 
+}
