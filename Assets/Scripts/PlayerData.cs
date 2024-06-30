d034b1c4f3997f6e2adbf89ad6102482f7c17fcc
Anas Sallam <Anas.Sallam001@umb.edu>
2024-06-29 23:24:51 -0400

    Update PlayerData.cs

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerData : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int playerID;
+    // public int playerID;
     public int health;
     public Vector3 position;
 
